f7f99be3b045775bc249665b8ceba4dfeb27b83e
owner1 <seyoungsong2017@gmail.com>
2021-11-15 18:09:20 -0500

    install tekton trigger

--- tekton/install.sh
+++ tekton/install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 kubectl apply --filename https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
+kubectl apply --filename https://storage.googleapis.com/tekton-releases/triggers/latest/release.yaml
+kubectl apply --filename https://storage.googleapis.com/tekton-releases/triggers/latest/interceptors.yaml
