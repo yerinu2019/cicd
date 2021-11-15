f4c4063ad613f47a1c16b4aa848d5db2b704efb9
owner1 <seyoungsong2017@gmail.com>
2021-11-15 12:14:06 -0500

    set dockerhub server

--- tekton/opa-envoy-plugin/dockerhub-secret.sh
+++ tekton/opa-envoy-plugin/dockerhub-secret.sh
@@ -2,7 +2,7 @@
 # create kubernetes secret for docker hub
 kubectl delete secret dockerhub-credential
 kubectl create secret docker-registry dockerhub-credential \
---docker-server=${DOCKER_HUB_SERVER} \
+--docker-server=https://index.docker.io/v1/ \
 --docker-username=${DOCKER_HUB_USER} \
 --docker-password=${DOCKER_HUB_PASSWORD} \
 --docker-email=${DOCKER_HUB_EMAIL}
