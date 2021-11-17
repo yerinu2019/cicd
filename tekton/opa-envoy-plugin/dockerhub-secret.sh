ae2fdb5416a83651e2261c81453c870e8f440427
owner1 <seyoungsong2017@gmail.com>
2021-11-17 16:00:05 -0500

    fix dockerhub-credential

--- tekton/opa-envoy-plugin/dockerhub-secret.sh
+++ tekton/opa-envoy-plugin/dockerhub-secret.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 # create kubernetes secret for docker hub
 kubectl delete secret dockerhub-credential
-
-export DOCKER_HUB_USER=$1
-set +H
-export DOCKER_HUB_PASSWORD=$2
-set -H
-export DOCKER_HUB_EMAIL=$3
-
 kubectl create secret docker-registry dockerhub-credential \
 --docker-server=https://index.docker.io/v1/ \
 --docker-username=${DOCKER_HUB_USER} \
