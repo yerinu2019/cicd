a483b73776dc32d623091df56839b610e80a83f3
owner1 <seyoungsong2017@gmail.com>
2021-11-17 14:23:09 -0500

    gke cicd cluster install and cleanup

--- tekton/opa-envoy-plugin/trigger/gitlab/gke-install.sh
+++ tekton/opa-envoy-plugin/trigger/gitlab/gke-install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+gcloud config set project monorepotest-323514
+gcloud config set compute/zone us-west1-a
+gcloud config set compute/region us-west1
+
+gcloud container clusters create cicd --num-nodes=1
+gcloud container clusters get-credentials cicd
+
 ./install.sh
 kubectl apply -f gke-ingress.yaml
 kubectl get ingress el-gitlab-listener-ingress
