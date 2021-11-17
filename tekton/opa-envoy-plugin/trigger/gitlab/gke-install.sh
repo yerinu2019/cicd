601561b45a67d2fb1e5213f56e6d360e62676377
owner1 <seyoungsong2017@gmail.com>
2021-11-17 14:52:04 -0500

    nginx ingress install

--- tekton/opa-envoy-plugin/trigger/gitlab/gke-install.sh
+++ tekton/opa-envoy-plugin/trigger/gitlab/gke-install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 ./install.sh
+
+kubectl create clusterrolebinding cluster-admin-binding \
+  --clusterrole cluster-admin \
+  --user $(gcloud config get-value account)
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.0.5/deploy/static/provider/cloud/deploy.yaml
+
 kubectl apply -f gke-ingress.yaml
 kubectl get ingress el-gitlab-listener-ingress
