f14d598b53f2c71056ac704e928136e82fbf95f6
owner1 <seyoungsong2017@gmail.com>
2021-11-19 11:45:13 -0500

    expose argocd server

--- argocd/install.sh
+++ argocd/install.sh
@@ -6,6 +6,7 @@ kubectl config use-context gke_monorepotest-323514_us-west1-a_cicd
 
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
 
 echo "kubectl get ingress -n argocd"
 kubectl get ingress -n argocd
