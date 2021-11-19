182d8eb56d713bdd59772296021bfd5a2e0d0395
owner1 <seyoungsong2017@gmail.com>
2021-11-19 11:58:22 -0500

    expose argocd server

--- argocd/install.sh
+++ argocd/install.sh
@@ -13,6 +13,7 @@ kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}
 
 kubectl -n argocd get all
 
+echo "Initial admin secret"
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
 
 
