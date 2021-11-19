9d7559bdc516e20081693cea497b7616066163e1
owner1 <seyoungsong2017@gmail.com>
2021-11-19 11:20:06 -0500

    gke cluster opa

--- gke/install.sh
+++ gke/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 source ../bash/gke-func.sh
 create-gke "monorepotest-323514" "us-west1-a" "us-west1" "cicd"
+create-gke "monorepotest-323514" "us-west1-a" "us-west1" "opa"
 
 #gcloud container clusters create opa-authz --num-nodes=1
 #gcloud container clusters get-credentials opa-authz
