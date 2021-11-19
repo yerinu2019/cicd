46d725d5af34a7029dc6b2bb126c537132be9521
owner1 <seyoungsong2017@gmail.com>
2021-11-19 16:06:17 -0500

    install with istio script

---
+++ argocd/istio-install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+./create-cluster.sh
+
+kubectl config use-context gke_monorepotest-323514_us-west1-a_cicd
+kubectl apply -k argocd-bootstrap
+
+echo "Expose argocd api server"
+kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+
+kubectl -n argocd get all
+
+echo "Initial admin secret"
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
