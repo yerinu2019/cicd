7fcdcb238c7a87c9faaa0696856cbfaa6e88673e
owner1 <seyoungsong2017@gmail.com>
2021-11-19 10:32:36 -0500

    install gke cluster(s)

--- gke/install.sh
+++ gke/install.sh
@@ -3,8 +3,15 @@ gcloud config set project monorepotest-323514
 gcloud config set compute/zone us-west1-a
 gcloud config set compute/region us-west1
 
-gcloud container clusters create cicd --num-nodes=1
-gcloud container clusters get-credentials cicd
+CICD=gcloud containsr clusters list | grep cicd
+if [[ -z "${CICD}" ]]; then
+  echo "Create GKE CICD"
+  #gcloud container clusters create cicd --num-nodes=1
+  #gcloud container clusters get-credentials cicd
+else
+  echo "GKE CICD exists."
+fi
+
 
 #gcloud container clusters create opa-authz --num-nodes=1
 #gcloud container clusters get-credentials opa-authz
