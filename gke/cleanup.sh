5fd8991948e23ad7fbdd71381cf6ee02fab40b62
owner1 <seyoungsong2017@gmail.com>
2021-11-19 11:27:31 -0500

    create cluster in background job

--- gke/cleanup.sh
+++ gke/cleanup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source ../bash/gke-func.sh
-delete-gke "cicd" "us-west1-a"
+delete-gke "cicd" "us-west1-a" &
 delete-gke "opa" "us-west1-a"
--- gke/install.sh
+++ gke/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source ../bash/gke-func.sh
-create-gke "monorepotest-323514" "us-west1-a" "us-west1" "cicd"
+create-gke "monorepotest-323514" "us-west1-a" "us-west1" "cicd" &
 create-gke "monorepotest-323514" "us-west1-a" "us-west1" "opa"
 
 #gcloud container clusters create opa-authz --num-nodes=1
