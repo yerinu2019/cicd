a1d8f3db4ccbe79b9e7df7a5396bb71eb4486b7c
owner1 <seyoungsong2017@gmail.com>
2021-11-20 18:33:28 -0500

    add source volume

--- tekton/authz/install.sh
+++ tekton/authz/install.sh
@@ -6,6 +6,6 @@ kubectl create clusterrolebinding cluster-admin-binding \
   --clusterrole cluster-admin \
   --user $(gcloud config get-value account)
 
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.0.5/deploy/static/provider/cloud/deploy.yaml
-kubectl apply -f gke-ingress.yaml
+#kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.0.5/deploy/static/provider/cloud/deploy.yaml
+#kubectl apply -f gke-ingress.yaml
 kubectl get ingress authz-gitlab-listener-ingress
