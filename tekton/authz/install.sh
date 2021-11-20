ccab0e028e32e0eb7717f1a2488400fbff86caa6
owner1 <seyoungsong2017@gmail.com>
2021-11-20 18:04:16 -0500

    yaml troubleshoot

--- tekton/authz/install.sh
+++ tekton/authz/install.sh
@@ -7,4 +7,5 @@ kubectl create clusterrolebinding cluster-admin-binding \
   --user $(gcloud config get-value account)
 
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.0.5/deploy/static/provider/cloud/deploy.yaml
+kubectl apply -f gke-ingress.yaml
 kubectl get ingress authz-gitlab-listener-ingress
