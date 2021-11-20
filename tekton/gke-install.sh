2b6fdc8812a1892e316e26959987756169eba3a2
owner1 <seyoungsong2017@gmail.com>
2021-11-20 17:36:40 -0500

    refactoring

--- tekton/gke-install.sh
+++ tekton/gke-install.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-gcloud config set project monorepotest-323514
-gcloud config set compute/zone us-west1-a
-gcloud config set compute/region us-west1
-
-gcloud container clusters create cicd --num-nodes=1
-gcloud container clusters get-credentials cicd
+source ../bash/gke-func.sh
+create-gke "monorepotest-323514" "us-west1-a" "us-west1" "cicd"
 
 ./install.sh
