38a10915de5144ce9a90e7afa85eb7cbfe919f0b
owner1 <seyoungsong2017@gmail.com>
2021-11-20 17:28:49 -0500

    authz rego build gitlab integration

---
+++ tekton/authz/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+kubectl apply -f gitlab-listener.yaml
+
+kubectl create clusterrolebinding cluster-admin-binding \
+  --clusterrole cluster-admin \
+  --user $(gcloud config get-value account)
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.0.5/deploy/static/provider/cloud/deploy.yaml
+
+kubectl get ingress authz-gitlab-listener-ingress
