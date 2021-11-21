5d45bee4e119235375c8773b54d2db2227c7a85c
owner1 <seyoungsong2017@gmail.com>
2021-11-21 16:04:10 -0500

    expose tekton dashboard

--- setup/tekton.sh
+++ setup/tekton.sh
@@ -4,4 +4,5 @@ kubectl apply --filename https://storage.googleapis.com/tekton-releases/pipeline
 kubectl apply --filename https://storage.googleapis.com/tekton-releases/triggers/latest/release.yaml
 kubectl apply --filename https://storage.googleapis.com/tekton-releases/triggers/latest/interceptors.yaml
 kubectl apply --filename https://storage.googleapis.com/tekton-releases/dashboard/latest/tekton-dashboard-release.yaml
+kubectl annotate -n tekton-pipelines svc tekton-dashboard external-dns.alpha.kubernetes.io/hostname=tekton.svc.yerinu.com.
 kubectl apply -f tekton-rbac.yaml
