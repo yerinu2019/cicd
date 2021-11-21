8d6285625f37ae7249b16cdbf87693a75f7031c2
owner1 <seyoungsong2017@gmail.com>
2021-11-21 16:47:15 -0500

    patch tekton dashboard

--- setup/tekton.sh
+++ setup/tekton.sh
@@ -3,5 +3,6 @@
 kubectl apply --filename https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
 kubectl apply --filename https://storage.googleapis.com/tekton-releases/triggers/latest/release.yaml
 kubectl apply --filename https://storage.googleapis.com/tekton-releases/triggers/latest/interceptors.yaml
-kubectl apply -k tekton
+kubectl apply -k https://storage.googleapis.com/tekton-releases/dashboard/latest/tekton-dashboard-release.yaml
+kubectl -k tekton
 kubectl apply -f tekton-rbac.yaml
