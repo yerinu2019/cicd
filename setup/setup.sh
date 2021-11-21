1f7723bd22b0807bc0c531ed4f1f4053de5f4c0c
owner1 <seyoungsong2017@gmail.com>
2021-11-20 22:17:53 -0500

    main setup script

--- setup/setup.sh
+++ setup/setup.sh
@@ -13,3 +13,43 @@ sudo apt update;sudo apt install -y gnupg
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3EFE0E0A2F2F60AA
 echo "deb http://ppa.launchpad.net/tektoncd/cli/ubuntu eoan main"|sudo tee /etc/apt/sources.list.d/tektoncd-ubuntu-cli.list
 sudo apt update && sudo apt install -y tektoncd-cli
+
+echo "Create GKE cicd"
+source ../bash/gke-func.sh
+create-gke "monorepotest-323514" "us-west1-a" "us-west1" "cicd"
+
+echo "Install tekton in cicd GKE"
+kubectl apply --filename https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
+kubectl apply --filename https://storage.googleapis.com/tekton-releases/triggers/latest/release.yaml
+kubectl apply --filename https://storage.googleapis.com/tekton-releases/triggers/latest/interceptors.yaml
+
+echo "Configure tekton rbac"
+kubectl apply -f tekton-rbac.yaml
+
+echo "Configure authz gitlab listener"
+kubectl apply -f ../tekton/authz/gitlab-listener.yaml
+
+echo "Install argocd cli"
+curl -sSL -o $HOME/bin/argocd https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64 && chmod +x $HOME/bin/argocd
+
+echo "Install argocd in cicd GKE"
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+echo "Expose argocd api server"
+kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+
+echo "Check argocd server external IP address"
+kubectl -n argocd get all
+
+echo "Check initial argocd admin secret"
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+echo
+
+echo "Install authz policy"
+kubectl apply -k ../argocd/applications/authz/base
+
+echo "Install ingress nginx"
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.0.5/deploy/static/provider/cloud/deploy.yaml
+echo "Configure ingress to authz and opa-envoy-plugin gitlab listener"
+kubectl apply -f gke-ingress.yaml
