c09dc670e1d445a0efeec9984ee5712a8a3d51cc
owner1 <seyoungsong2017@gmail.com>
2021-11-20 21:14:07 -0500

    opa version change

--- setup/setup.sh
+++ setup/setup.sh
@@ -5,6 +5,6 @@ if [ ! -d "$HOME/bin" ]; then
 fi
 
 echo "Install opa"
-curl -L -o $HOME/bin/opa https://github.com/open-policy-agent/opa/releases/download/v0.11.0/opa_linux_amd64
+curl -L -o $HOME/bin/opa https://github.com/open-policy-agent/opa/releases/download/v0.34.2/opa_linux_amd64
 chmod 755 $HOME/bin/opa
 
