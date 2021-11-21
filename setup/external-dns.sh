85866e73b0383d83f9562dc0bdd1c00e7889bbd3
owner1 <seyoungsong2017@gmail.com>
2021-11-21 15:27:59 -0500

    fix googleapi dns server

--- setup/external-dns.sh
+++ setup/external-dns.sh
@@ -6,7 +6,7 @@ gcloud dns managed-zones create "svc-yerinu-com" \
 
 echo "Tell the parent zone where to find the DNS records for this zone by adding the corresponding NS records there."
 gcloud dns record-sets transaction start --zone "yerinu-com"
-gcloud dns record-sets transaction add ns-cloud-c{1..4}.googledomains.com. \
+gcloud dns record-sets transaction add ns-cloud-b{1..4}.googledomains.com. \
     --name "svc.yerinu.com." --ttl 300 --type NS --zone "yerinu-com"
 gcloud dns record-sets transaction execute --zone "yerinu-com"
 
