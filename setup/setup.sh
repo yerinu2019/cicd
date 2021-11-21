a6ed639df7a307333f192cd031e80cbc338c5ee6
owner1 <seyoungsong2017@gmail.com>
2021-11-20 21:50:13 -0500

    install tekton client

--- setup/setup.sh
+++ setup/setup.sh
@@ -8,3 +8,8 @@ echo "Install opa"
 curl -L -o $HOME/bin/opa https://github.com/open-policy-agent/opa/releases/download/v0.34.2/opa_linux_amd64
 chmod 755 $HOME/bin/opa
 
+echo "Install tekton cli"
+sudo apt update;sudo apt install -y gnupg
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3EFE0E0A2F2F60AA
+echo "deb http://ppa.launchpad.net/tektoncd/cli/ubuntu eoan main"|sudo tee /etc/apt/sources.list.d/tektoncd-ubuntu-cli.list
+sudo apt update && sudo apt install -y tektoncd-cli
