6c574ff23846b57aa13e41584040c4325bdac3a9
owner1 <seyoungsong2017@gmail.com>
2021-11-22 00:23:48 -0500

    argocd has to be installed in the argocd namespace

--- setup/argocd-server.sh
+++ setup/argocd-server.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+kubectl apply -f argocd/namespace.yaml
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl apply -k argocd
