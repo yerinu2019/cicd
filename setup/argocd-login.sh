94d21b82e7e7156f6a3350d872835ea9015032ed
owner1 <seyoungsong2017@gmail.com>
2021-11-22 17:03:16 -0500

    argocd login

--- setup/argocd-login.sh
+++ setup/argocd-login.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+# login to argocd using the initial admin secret
 argocd login --username admin --password `kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d`
