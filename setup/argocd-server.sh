c479de16f749693ba55162f17fc06a1aec3b299a
owner1 <seyoungsong2017@gmail.com>
2021-11-22 10:11:01 -0500

    argocd server install troubleshooting

--- setup/argocd-server.sh
+++ setup/argocd-server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 kubectl apply -f argocd/namespace.yaml
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-kubectl -n argocd patch service argocd-server --patch "$(cat patch-file.yaml)"
+kubectl -n argocd patch service argocd-server --patch-file patch.yaml
