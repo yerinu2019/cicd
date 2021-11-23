579481c07a6bab15dcdd14260676bc8dee485199
owner1 <seyoungsong2017@gmail.com>
2021-11-23 18:10:46 -0500

    TODO comment

--- setup/argo-events.sh
+++ setup/argo-events.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 kubectl apply -k argo-events
 kubectl apply -n argo-events -f https://raw.githubusercontent.com/argoproj/argo-events/master/manifests/install-validating-webhook.yaml
+
+#TODO: Disable argo events webhook. argo event is installed on all gke clusters. Automatic external dns
+# might have problems if multiple argo event webhook service is exposed to cloud dns entries using
+# the same host name.
