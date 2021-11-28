a679a129e41c4a05c8769c728ec33c6ec4419675
owner1 <seyoungsong2017@gmail.com>
2021-11-28 15:20:25 -0500

    restart authz-operator after auth-acl configuration change

--- setup/authz-acl.sh
+++ setup/authz-acl.sh
@@ -8,7 +8,9 @@ argocd cluster add gke_monorepotest-323514_us-central1-a_cluster1 -y
 
 kubectl apply -f ./authz-acl/argocd-app.yaml
 argocd app wait authz
-#kubectl -n api-istio rollout restart deployment authz-operator
-#kubectl -n api-istio rollout status deployment/authz-operator
-#kubectl get clusterrolebindings | grep api-istio
-#kubectl -n api-istio get rolebindings
+
+./gke-switch-cluster.sh cluster1
+kubectl -n api-istio rollout restart deployment authz-operator
+kubectl -n api-istio rollout status deployment/authz-operator
+kubectl get clusterrolebindings | grep api-istio
+kubectl -n api-istio get rolebindings
