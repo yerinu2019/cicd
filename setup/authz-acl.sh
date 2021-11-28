e3bac1a88f795da05b6f096d6e96309a5527123a
owner1 <seyoungsong2017@gmail.com>
2021-11-28 11:48:50 -0500

    do not use cluster1 for now

--- setup/authz-acl.sh
+++ setup/authz-acl.sh
@@ -3,7 +3,8 @@ source ../bash/gke-func.sh
 
 switch-gke "cicd"
 ./argocd-login.sh
-argocd cluster add gke_monorepotest-323514_us-central1-a_cluster1 -y
+#Uncomment argocd cluster when using cluster1
+#argocd cluster add gke_monorepotest-323514_us-central1-a_cluster1 -y
 
 kubectl apply -f ./authz-acl/argocd-app.yaml
 kubectl -n api-istio rollout restart deployment authz-operator
