691282e127765b9c2c3e0170ee9507fd3c4b52a8
owner1 <seyoungsong2017@gmail.com>
2021-12-02 19:53:18 -0500

    install argocd applicationset controller

--- setup/argocd-server.sh
+++ setup/argocd-server.sh
@@ -2,3 +2,4 @@
 kubectl apply -f argocd/namespace.yaml
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl -n argocd patch service argocd-server --patch-file argocd/patch.yaml
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj-labs/applicationset/v0.2.0/manifests/install.yaml
