42f6074a089e300a9a2b131dcf5d08fa8570373d
owner1 <seyoungsong2017@gmail.com>
2021-12-03 23:02:08 -0500

    install authz-client

---
+++ setup/auth-client.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#!/bin/bash
+source ../bash/gke-func.sh
+
+switch-gke "cicd"
+./argocd-login.sh
+#Uncomment argocd cluster when using cluster1
+argocd cluster add gke_monorepotest-323514_us-central1-a_cluster1 -y
+
+kubectl apply -f ../authz/graphql/argocd/authz-client.yaml
