d4158565f896f57bec525be0c2e9d802b731d607
owner1 <seyoungsong2017@gmail.com>
2021-12-03 22:55:25 -0500

    delete kustomize based authz-opa-istio

--- setup/authz-opa.sh
+++ setup/authz-opa.sh
@@ -8,7 +8,6 @@ argocd cluster add gke_monorepotest-323514_us-central1-a_cluster1 -y
 
 kubectl apply -f ../authz/graphql/argocd/authz-istio-envoy-filter.yaml
 kubectl apply -f ../authz/graphql/argocd/authz-opa-istio.yaml
-#kubectl apply -k ../authz/graphql/argocd/authz-opa-istio/overlays/gke_monorepotest-323514_us-central1-a_cluster1/api-istio
 kubectl apply -f ../authz/graphql/argocd/authz-gcs-egress.yaml
 kubectl apply -f ../authz/graphql/argocd/authz-rego-configmap.yaml
 
