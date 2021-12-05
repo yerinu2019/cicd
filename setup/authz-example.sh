b1e940b63a0ecb95125dd10966b5e5eafe584679
owner1 <seyoungsong2017@gmail.com>
2021-12-05 11:47:42 -0500

    merge authz-acl and authz-client into authz-example

--- setup/authz-example.sh
+++ setup/authz-example.sh
@@ -6,4 +6,4 @@ switch-gke "cicd"
 #Uncomment argocd cluster when using cluster1
 argocd cluster add gke_monorepotest-323514_us-central1-a_cluster1 -y
 
-kubectl apply -f ../authz/graphql/argocd/authz-acl.yaml
+kubectl apply -f ../authz/graphql/argocd/authz-example.yaml
