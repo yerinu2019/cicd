8b656e89bf05734431747d0ef4d5ed56a6c066a0
owner1 <seyoungsong2017@gmail.com>
2021-12-05 16:53:57 -0500

    restart api1 and api2 after syncing authz-example

--- setup/authz-example.sh
+++ setup/authz-example.sh
@@ -7,3 +7,7 @@ switch-gke "cicd"
 argocd cluster add gke_monorepotest-323514_us-central1-a_cluster1 -y
 
 kubectl apply -f ../authz/graphql/argocd/authz-example.yaml
+
+switch-gke "cluster1"
+kubectl -n api-istio rollout restart deployment api1
+kubectl -n api-istio rollout restart deployment api2
