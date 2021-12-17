8be3ca6e7c4643e3eab2031fa457a084bce70cb0
owner1 <seyoungsong2017@gmail.com>
2021-12-17 17:28:04 -0500

    create bigquery decision log table

--- authz/graphql/logging/bq/create-table.sh
+++ authz/graphql/logging/bq/create-table.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-CURRENT_DIR=$(pwd)
 bq mk \
   --table \
   --expiration 3600 \
   --description "OPA decision log table" \
   monorepotest-323514:authz.decision_log \
-  ${CURRENT_DIR}/schema.json
+  ./schema.json
