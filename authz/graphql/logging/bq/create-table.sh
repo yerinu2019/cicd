f7a0f6e9e0a78f396027399b85e433fad7232a0e
owner1 <seyoungsong2017@gmail.com>
2021-12-18 17:55:22 -0500

    decision log table schema update

--- authz/graphql/logging/bq/create-table.sh
+++ authz/graphql/logging/bq/create-table.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 DIR=$(pwd)
-bq mk \
-  --table \
-  --expiration 3600 \
-  --description "OPA decision log table" \
-  monorepotest-323514:authz.decision_log \
-  ${DIR}/schema.json
+CHECK=$(bq ls authz | grep decision_log)
+if [[ -z $"$CHECK" ]]; then
+  bq mk \
+    --table \
+    --expiration 3600 \
+    --description "OPA decision log table" \
+    monorepotest-323514:authz.decision_log \
+    ${DIR}/schema.json
+else
+  bq update monorepotest-323514:authz.decision_log ${DIR}/schema.json
+fi
+
 
 cd ../adapter
 ./make.sh
